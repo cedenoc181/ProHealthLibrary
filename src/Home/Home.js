b10ec105f42b2d633a8110ccad53c2947f5589cc
Christian <cedenoc181@gmail.com>
2023-05-04 13:35:25 -0400

    finalized the ui og home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css"
 
+
 function Home() {
   return (
     <div className="bg-gray-100 w-fill h-screen">
@@ -8,8 +9,6 @@ function Home() {
             <p className="p-10 font-semibold text-gray-600 text-3xl shadow-2xl">
                  Welcome to Pro Health and Fitness PDF library, This application allows therapists and administrative staff to quickly access the documents they need. 
                  It serves as a centralized repository for all the PDF documents needed for clinical operations, such as patient intake forms, APOS treatment plans, Direct Access form, APOS follow up questionaire and much more. 
-                 This not only saves time but also ensures that important documents are organized and readily available when needed. 
-                 Overall, your PDF library is a valuable asset that helps streamline clinical operations and improve patient care.
             </p>
         </div>
 
@@ -17,6 +16,10 @@ function Home() {
             <a href="/PTOT"><button className="border border-blue-600 border-2 p-1 text-blue-500 hover:text-blue-800 hover:bg-gray-300 rounded font-semibold cursor-pointer">PT OT PDF </button></a>
             <a href="/APOS"><button className="border border-blue-600 border-2 p-1 text-blue-500 hover:text-blue-800 hover:bg-gray-300 rounded font-semibold cursor-pointer">APOS therapy PDF</button></a>
             <a href="/Auth"><button className="border border-blue-600 border-2 p-1 text-blue-500 hover:text-blue-800 hover:bg-gray-300 rounded font-semibold cursor-pointer">Authorization PDF</button></a>
+        </div>
+        <div className="flex justify-evenly">
+          <img className="w-250 h-150" src={require ("../PTimgRBG.png")} alt="fdimg"/>
+          <img className="" src={require ("../ptimgphfRBG.png")} alt="fdimg2"/>
         </div>
     </div>
   )
