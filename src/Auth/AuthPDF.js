e68960f364a4b00c65cdc7e00238a348d3d9c2af
Christian <cedenoc181@gmail.com>
2023-05-09 13:40:22 -0400

    added structure to db json

--- src/Auth/AuthPDF.js
+++ src/Auth/AuthPDF.js
@@ -1,30 +1,12 @@
 import React from 'react'
-// import { Viewer } from '@react-pdf-viewer/core';
 import AFAPOS from "./Auth-files/all forms 2022.pdf"
-
-// Plugins
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-
-// // Import styles
-// import '@react-pdf-viewer/core/lib/styles/index.css';
-// import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 
 function AuthPDF() {
-  // Create new plugin instance
-// const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
     <div className="bg-gray-100 w-fill h-screen">
-<iframe id="fred" style={{border:"1px"}} title="PDF in an i-Frame" src={AFAPOS} frameborder="1" scrolling="auto" height="1100" width="850" ></iframe>
-
-{/* <Viewer
-    fileUrl={AFAPOS}
-    plugins={[
-        // Register plugins
-        defaultLayoutPluginInstance
-      
-    ]}
-/> */}
+<iframe id="fred" style={{border:"1px"}} title="PDF in an i-Frame" src={AFAPOS} frameborder="1" scrolling="auto" height="100%" width="100%" ></iframe>
 
     </div>
   )
