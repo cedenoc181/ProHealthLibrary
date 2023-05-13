692a36386d695eefb920738cda1e2fa79c1b67ee
Christian <cedenoc181@gmail.com>
2023-05-13 17:51:57 -0400

    adding single pdf images now

--- src/Other/Other.js
+++ src/Other/Other.js
@@ -4,6 +4,13 @@ import  OtherPDF from "./OtherPDF"
 function Other() {
   return (
     <div className="bg-gray-100 w-fill h-screen">
+    <div className="Prompt">
+          <p className="p-10 font-semibold text-gray-600 text-3xl text-center">
+            Single page PDF's, APOS forms, direct access form, financial hardship form and more can be found here in both english and spanish.
+          </p>
+    </div>  
+
+
 
       <OtherPDF />
       
--- src/Other/OtherPDF.js
+++ src/Other/OtherPDF.js
@@ -1,34 +1,15 @@
-import React, {useState} from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import React from 'react';
+
 // import FHF from "./Other-files/Fhard.pdf";
 
-function OtherPDF({pdfFiles}) {
+function OtherPDF() {
 
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
 
-  console.log(pdfFiles)
-  
-  function onDocumentSuccess({numPages}) {
-    setNumPages(numPages)
-    }
 
 
   return (
     <div className="bg-gray-100 w-fill h-screen">
 
-<Document file="" onLoadSuccess={onDocumentSuccess}>
-    <Page 
-    pageNumber={pageNumber}
-    renderTextLayer={false}
-    renderAnnotationLayer={false}
-    customTextRenderer={false}
-    />
-  </Document>
-
-    <p className="flex justify-evenly border border-success w-100">
-         Page {pageNumber} of {numPages}
-    </p> 
 
     </div>
   )
